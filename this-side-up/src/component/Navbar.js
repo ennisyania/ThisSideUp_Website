b0bd3430f8452c21e011508691262300d552034e
ennisyania <ennisyania@gmail.com>
2025-05-21 20:43:41 +0000

    tweaked routing

--- this-side-up/src/component/Navbar.js
+++ this-side-up/src/component/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import './Navbar.css';
+import { Link } from 'react-router-dom';
+
 
 const RightArrowIcon = () => (
   <svg
--- this-side-up/src/index.js
+++ this-side-up/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Homepage from './Homepage';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Homepage />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
