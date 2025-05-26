f6b538d3d39fb4c2b9018e49f7e1c23dac0f836e
ennisyania <ennisyania@gmail.com>
2025-05-26 23:22:59 +0000

    updated connection

--- this-side-up/backend/index.js
+++ this-side-up/backend/index.js
@@ -1,13 +1,9 @@
-require('dotenv').config(); // Load environment variables from .env
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
-const username = process.env.MONGO_USER;
-const password = encodeURIComponent(process.env.MONGO_PASS); // encode to safely handle special chars
-const cluster = process.env.MONGO_CLUSTER;
-const dbName = process.env.MONGO_DB;
+const mongoUri = process.env.MONGODB_URI;
 
-const uri = `mongodb+srv://${username}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected!'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(mongoUri)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
